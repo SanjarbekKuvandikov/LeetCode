4aa027176a774fe9801a8dbaa3ed6faf09220e3e
sanjarbek <quvandiqovsanjarbek@gmail.com>
2024-09-03 16:24:33 +0500

    TheBest Time Buy Sell problem solved [03.09.2024 16:23]

---
+++ src/TheBestTimeBuySell.java
@@ -0,0 +1,12 @@
+public class TheBestTimeBuySell {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+        for (int i = 0; i < prices.length; i++){
+             minPrice = Math.min(minPrice,prices[i]);
+             maxProfit = Math.max(maxProfit,prices[i] - minPrice);
+        }
+
+        return maxProfit;
+    }
+}
