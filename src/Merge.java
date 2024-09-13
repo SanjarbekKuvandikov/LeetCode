e969c2878fb583d6a1a4a9d589bd679594de99cf
sanjarbek <quvandiqovsanjarbek@gmail.com>
2024-09-13 15:12:28 +0500

    Changes [13.09.2024 15:11]

--- src/Merge.java
+++ src/Merge.java
@@ -28,18 +28,5 @@ public class Merge {
         }
     }
 
-    public static void main(String[] args) {
-        Merge merge = new Merge();
-        int[] nums1 = {2,0};
-        int m = 1;
-        int[] nums2 = {1};
-        int n = 1;
-        merge.merge(nums1,m,nums2,n);
-
-        for (int num : nums1) {
-            System.out.print(num + " ");
-        }
-        // Output: [1, 2, 2, 3, 5, 6]
-    }
 }
 
--- src/RemoveDuplicatesFromSortedArray.java
+++ src/RemoveDuplicatesFromSortedArray.java
@@ -24,12 +24,4 @@ public class RemoveDuplicatesFromSortedArray {
     }
 
 
-
-    public static void main(String[] args) {
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
-        RemoveDuplicatesFromSortedArray remove = new RemoveDuplicatesFromSortedArray();
-        System.out.println(remove.removeDuplicates(nums));
-        System.out.println(Arrays.toString(nums));
-    }
-
 }
