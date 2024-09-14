78fdc2f1e0f8ff70802742a50112ae2d9d2f640f
sanjarbek <quvandiqovsanjarbek@gmail.com>
2024-09-14 22:08:19 +0500

    Mountains problem solved2 [14.09.2024 14:43]

--- src/Mountains.kt
+++ src/Mountains.kt
@@ -1,43 +1,40 @@
-import java.util.LinkedList
-import java.util.Queue
+import java.util.*
 
 data class State(val x: Int, val y: Int, val health: Int)
 
-fun canReach(grid: List<List<Int>>, health: Int): Boolean {
+fun findSafeWalk(grid: List<List<Int>>, health: Int): Boolean {
     val m = grid.size
     val n = grid[0].size
 
-    // Directions for moving in the grid (up, down, left, right)
-    val directions = arrayOf(
+    val moves = arrayOf(
         intArrayOf(-1, 0), // up
         intArrayOf(1, 0),  // down
         intArrayOf(0, -1), // left
         intArrayOf(0, 1)   // right
     )
-
+    val newHealth = if (grid[0][0] == 1) health - 1 else health
     // BFS queue storing (x, y, remaining health)
     val queue: Queue<State> = LinkedList()
-    queue.offer(State(0, 0, health))
+    queue.offer(State(0, 0, newHealth))
 
     // Visited array to keep track of the remaining health at each cell
     val visited = Array(m) { IntArray(n) { -1 } }
-    visited[0][0] = health
+    visited[0][0] = newHealth
 
     while (queue.isNotEmpty()) {
         val current = queue.poll()
         val x = current.x
         val y = current.y
         var currentHealth = current.health
-
         // If we reach the bottom-right corner, return true
         if (x == m - 1 && y == n - 1) {
-            return currentHealth > 1
+            return currentHealth > 0
         }
 
         // Explore all four possible directions
-        for (dir in directions) {
-            val newX = x + dir[0]
-            val newY = y + dir[1]
+        for (move in moves) {
+            val newX = x + move[0]
+            val newY = y + move[1]
 
             // Check if the new position is within bounds
             if (newX in 0 until m && newY in 0 until n) {
@@ -51,28 +48,17 @@ fun canReach(grid: List<List<Int>>, health: Int): Boolean {
                 }
             }
         }
-    }
 
-    // If we exhaust all options without reaching the target, return false
+    }
     return false
 }
 
 fun main() {
-    // [[0,1,0,0,0],[0,1,0,1,0],[0,0,0,1,0]]
-    val grid1 = arrayOf(
-        intArrayOf(0,1,0,0,0),
-        intArrayOf(0,1,0,1,0),
-        intArrayOf(0,0,0,1,0)
+    val grid1 = listOf(
+        listOf(1,1,1,1)
     )
-    val health1 = 1
 
-    // [[0,1,1,0,0,0],[1,0,1,0,0,0],[0,1,1,1,0,1],[0,0,1,0,1,0]], health = 3
-    val grid2 = listOf(
-        listOf(0,1,1,0,0,0),
-        listOf(1,0,1,0,0,0),
-        intArrayOf(0,1,1,1,0,1),
-        intArrayOf(0,0,1,0,1,0)
-    )
-    val health2  = 3
+    val health1 = 5
+    println(findSafeWalk(grid1,health1))
 
 }
