6ca99df177be6b54d0170b63a78c151e4e9469e0
sanjarbek <quvandiqovsanjarbek@gmail.com>
2024-09-16 22:52:57 +0500

    Happy Number problem solved [16.09.2024 22:52]

---
+++ src/HappyNumber.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+public class HappyNumber {
+    public boolean isHappy(int n) {
+        Set<Integer> seenNumbers = new HashSet<>();
+
+        while (n != 1 && !seenNumbers.contains(n)) {
+            seenNumbers.add(n);
+            n = aylantiruvchi(n);
+        }
+
+        return n == 1;
+
+    }
+    public static int aylantiruvchi(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+}
