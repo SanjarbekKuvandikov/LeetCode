143340fe90f25434b189683333d953b335d48e1c
sanjarbek <quvandiqovsanjarbek@gmail.com>
2024-09-16 23:03:24 +0500

    example

--- src/HIndex.java
+++ src/HIndex.java
@@ -4,6 +4,7 @@ public class HIndex {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
         //0 1 3 5 6 length=5
+        // aaa
         for (int i = 0; i < citations.length; i++) {
             if (citations[i]>=citations.length - i) return citations.length - i;
         }
