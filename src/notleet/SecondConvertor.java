11c42255f03d51744458e89d3cf8e260399b0efd
sanjarbek <quvandiqovsanjarbek@gmail.com>
2024-09-25 14:17:01 +0500

    Second Convertor problem solved [25.09.2024 14:16]

---
+++ src/notleet/SecondConvertor.java
@@ -0,0 +1,12 @@
+package notleet;
+
+public class SecondConvertor {
+    public String convertor(int sec){
+        int hours = (sec/ 3600) %24;
+        int minutes = (sec % 3600) / 60;
+        int seconds = sec % 60;
+
+        String time = String.format("%01d:%02d:%02d", hours, minutes, seconds);
+        return time;
+    }
+}
