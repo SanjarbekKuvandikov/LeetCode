1007b729f15aa38cdec1c762d24ad04fca8b2e28
sanjarbek <quvandiqovsanjarbek@gmail.com>
2024-09-25 20:59:41 +0500

    Epam.com GoDutch question solved [25.09.2024 18:16]

---
+++ src/com/epam/GoDutch.java
@@ -0,0 +1,21 @@
+package com.epam;
+
+import java.util.Scanner;
+
+public class GoDutch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int money = sc.nextInt();
+        int friends = sc.nextInt();
+        if (money < 0){
+            System.out.println("Bill total amount cannot be negative");
+        }else if (friends <= 0){
+            System.out.println("Number of friends cannot be negative or zero");
+        }else {
+            money += ((money * 10)/100);
+            money = money/friends;
+            System.out.println(money);
+        }
+    }
+}
