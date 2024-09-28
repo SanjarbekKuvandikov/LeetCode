5532e8430f6859859a6df806db0c9758cbad4394
sanjarbek <quvandiqovsanjarbek@gmail.com>
2024-09-28 20:38:16 +0500

    Leetcode MissingNumber question solved [28.09.2024 20:38]

---
+++ src/MissingNumber.java
@@ -0,0 +1,29 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MissingNumber {
+    public int missingNumber(int[] nums) {
+
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i != nums[i]) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
+/*Optimal solution.
+* public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum;
+    }
+*
+ */
