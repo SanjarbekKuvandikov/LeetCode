5102b9f11eabc582f3068532bd926312181e98ec
sanjarbek <quvandiqovsanjarbek@gmail.com>
2024-10-04 23:45:48 +0500

    Leetcode ThreeSumClosest questions solved [04.10.2024 23:45]

--- src/ThreeSumClosest.java
+++ src/ThreeSumClosest.java
@@ -5,11 +5,10 @@ public class ThreeSumClosest {
         int closestsum = nums[0] + nums[1] + nums[2];
         Arrays.sort(nums);
         for (int i = 0; i < nums.length-2; i++) {
-            int first = i;
             int left = i+1;
             int right = nums.length-1;
             while (left < right) {
-                int sum = nums[first] + nums[left] + nums[right];
+                int sum = nums[i] + nums[left] + nums[right];
                 if (sum == target) {
                     return sum;
                 }
