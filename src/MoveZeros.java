0f973fe6487fa2ad1bcf92079da3d58250248933
sanjarbek <quvandiqovsanjarbek@gmail.com>
2024-10-30 21:37:47 +0500

    Leetcode MoveZeros question solved

---
+++ src/MoveZeros.java
@@ -0,0 +1,15 @@
+
+public class MoveZeros {
+    public void moveZeroes(int[] nums) {
+        int pointer = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] != 0){
+                nums[pointer] = nums[i];
+                pointer++;
+            }
+        }
+        for (int i = pointer; i < nums.length; i++){
+            nums[i] = 0;
+        }
+    }
+}
