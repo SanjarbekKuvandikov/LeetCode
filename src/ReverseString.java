4fec4005b62f33e8c85387fe53816e04899cbe3e
sanjarbek <quvandiqovsanjarbek@gmail.com>
2024-11-02 22:57:27 +0500

    Leetcode ReverseString question solved

---
+++ src/ReverseString.java
@@ -0,0 +1,13 @@
+public class ReverseString {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length-1;
+        while (left < right){
+            var temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
