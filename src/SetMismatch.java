77a981bec47840c54a609ae07f617f594e207671
sanjarbek <quvandiqovsanjarbek@gmail.com>
2024-11-23 21:35:59 +0500

    Leetcode SetMismatch question solved

---
+++ src/SetMismatch.java
@@ -0,0 +1,21 @@
+public class SetMismatch {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int[] count = new int[n + 1];
+        int duplicate = -1, missing = -1;
+
+        for (int num : nums) {
+            count[num]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (count[i] == 2) {
+                duplicate = i;
+            } else if (count[i] == 0) {
+                missing = i;
+            }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+}
