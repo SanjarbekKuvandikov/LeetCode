3d2c74f33c135ccb1991b87ed84c91d5041dc4fa
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-03-13 14:55:51 +0500

    Leetcode 121 question solved

---
+++ src/BestTimeToBuy.java
@@ -0,0 +1,16 @@
+public class BestTimeToBuy {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxPrice = 0;
+
+        for (int price : prices){
+            if (price < minPrice){
+                minPrice = price;
+            }else {
+                maxPrice = Math.max(maxPrice,price - minPrice);
+            }
+        }
+
+        return maxPrice;
+    }
+}
