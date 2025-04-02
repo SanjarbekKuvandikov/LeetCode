97bf79ba9f53991fbf7aa3e1b8bc8e47221e7c4b
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-04-02 18:10:14 +0500

    Leetcode 195 question solved

---
+++ src/TenthLine.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TenthLine {
+   /* String userHome = System.getProperty("user.home");
+
+    File desktopPath = new File(userHome,"Desktop");
+
+    String name = "Line.txt";
+
+    File file = new File(desktopPath, name);
+
+
+        if (file.exists()){
+        System.out.println("Success" + file.getAbsolutePath());
+        int counter = 1;
+        try (BufferedReader br = new BufferedReader(new FileReader(file))){
+            String line;
+            while ((line = br.readLine()) != null){
+
+                if (counter == 10){
+                    System.out.println(line);
+                    break;
+                }
+                counter++;
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+        else {
+        System.out.println("NonSuccess");
+    }*/
+}
