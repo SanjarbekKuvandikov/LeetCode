303e611c9563fcaa749b8ec83175219618e4c517
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-04-23 12:22:35 +0500

    LeetCode 1920 question solved

---
+++ src/BuildArrayFromPermutation.java
@@ -0,0 +1,13 @@
+public class BuildArrayFromPermutation {
+    public int[] buildArray(int[] nums) {
+        int length = nums.length;
+        int [] ans = new  int[length];
+
+        for (int i = 0; i < length; i++){
+
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
+}
