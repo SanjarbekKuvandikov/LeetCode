79f7e31615342eb9cf2b353846b7f509146da6aa
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-04-26 17:07:50 +0500

    LeetCode 3300 question solved

---
+++ src/TheTwoSneakyNumbersDigitville.java
@@ -0,0 +1,19 @@
+public class TheTwoSneakyNumbersDigitville {
+    public int minElement(int[] nums) {
+        int min = circle(nums[0]);
+
+        for (int i = 1; i < nums.length; i++){
+            min = Math.min(min,circle(nums[i]));
+        }
+        return min;
+    }
+    private int circle(int n){
+        int sum = 0;
+        while (n > 0){
+            int f = n % 10;
+            sum += f;
+            n = n/10;
+        }
+        return sum;
+    }
+}
