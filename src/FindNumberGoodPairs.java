3c106e3f1a65fbb36563b555e4153ce30b83f29c
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-05-02 19:16:47 +0500

    LeetCode 3162 question solved

---
+++ src/FindNumberGoodPairs.java
@@ -0,0 +1,13 @@
+public class FindNumberGoodPairs {
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        int count = 0;
+        for (int value : nums1) {
+            for (int i : nums2) {
+                if (value % (i * k) == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
