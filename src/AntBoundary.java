fcc109dc25824abf26bc34d36618622308ca93e1
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-05-05 17:57:47 +0500

    LeetCode 3028 question solved

---
+++ src/AntBoundary.java
@@ -0,0 +1,15 @@
+public class AntBoundary {
+    public int returnToBoundaryCount(int[] nums) {
+        int count = 0;
+        int sum = 0;
+        for (int num: nums){
+            sum += num;
+
+            if (sum == 0){
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
