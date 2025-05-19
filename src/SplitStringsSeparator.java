6c6765ee2f0435f171241672a73fc272097a28f3
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-05-19 15:37:49 +0500

    Leetcode 2788 question solved

---
+++ src/SplitStringsSeparator.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SplitStringsSeparator {
+    public List<String> splitWordsBySeparator(List<String> words, char separator) {
+        List<String> result = new ArrayList<>();
+
+        for (String word : words){
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < word.length(); i++){
+                if (separator != word.charAt(i)){
+                    str.append(word.charAt(i));
+                }else {
+                    if (!str.isEmpty()){
+                        result.add(str.toString());
+                        str.setLength(0);
+                }
+                }
+            }
+
+            if (!str.isEmpty()){
+                result.add(str.toString());
+            }
+
+        }
+
+
+
+        return result;
+    }
+}
