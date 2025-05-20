9a645f611dc2e51e826370d0d515d4d12793f258
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-05-20 14:21:17 +0500

    Leetcode 2716 question solved

---
+++ src/MinimizeStringLength.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class MinimizeStringLength {
+    public int minimizedStringLength(String s) {
+        Set<Character> set = new HashSet<>();
+
+        for (char c : s.toCharArray()){
+            set.add(c);
+        }
+
+        return set.size();
+    }
+}
