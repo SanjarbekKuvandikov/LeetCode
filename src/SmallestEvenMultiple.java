bcf488b554a2bab21a5ca286a72375bec772a521
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-05-24 14:03:38 +0500

    Leetcode 2413 question solved

---
+++ src/SmallestEvenMultiple.java
@@ -0,0 +1,11 @@
+public class SmallestEvenMultiple {
+    public int smallestEvenMultiple(int n) {
+        for (int i = n; i <= 2 * n; i++){
+            if (i % n == 0 && i % 2 == 0){
+                return i;
+            }
+        }
+
+        return 0;
+    }
+}
