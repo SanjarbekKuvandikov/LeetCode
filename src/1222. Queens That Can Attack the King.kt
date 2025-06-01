2fbaa64aaa8ff528c5bf38c223b748ac90da388b
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-06-01 16:09:45 +0500

    Leetcode 1222 question solved

---
+++ src/1222. Queens That Can Attack the King.kt
@@ -0,0 +1,58 @@
+fun queensAttacktheKing(queens: Array<IntArray>, king: IntArray): List<List<Int>> {
+    val kingAttackQueens: MutableList<List<Int>> = mutableListOf()
+
+    val moves: Array<Pair<Int, Int>> = arrayOf(
+        Pair(0,-1),
+        Pair(0,1),
+        Pair(-1,0),
+        Pair(1,0),
+
+        Pair(-1,-1),
+        Pair(1,-1),
+        Pair(-1,1),
+        Pair(1,1),
+    )
+
+    for (q in queens){
+        val x = q[0]
+        val y = q[1]
+        var maxMoves = 8
+
+        while (maxMoves > 0){
+          for (m in moves){
+              val xMove = m.first * maxMoves
+              val yMove = m.second * maxMoves
+              if (xMove + x >= 0 && xMove + x < 8
+                  && yMove + y >= 0 && yMove + y < 8
+                  && king[0] == xMove + x && king[1] == yMove + y){
+                  kingAttackQueens.add(
+                      listOf(x,y)
+                  )
+              }
+          }
+           maxMoves--
+        }
+    }
+
+    return kingAttackQueens
+}
+
+fun main() {
+    //[[0,1],[1,0],[4,0],[0,4],[3,3],[2,4]], king = [0,0]
+    val ans = queensAttacktheKing(
+        queens = arrayOf(
+            intArrayOf(0,1),
+            intArrayOf(1,0),
+            intArrayOf(4,0),
+            intArrayOf(0,4),
+            intArrayOf(3,3),
+            intArrayOf(2,4)
+        ),
+        king = intArrayOf(0,0)
+    )
+
+    // [[0,1],[1,0],[3,3]]
+    // [[0, 1], [1, 0], [4, 0], [0, 4], [3, 3]]
+
+    println(ans.toString())
+}
