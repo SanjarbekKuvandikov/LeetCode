56821be0f31f9ef4d361e053f041b8a2158fdd65
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-06-01 18:12:30 +0500

    solved

--- src/1222. Queens That Can Attack the King.kt
+++ src/1222. Queens That Can Attack the King.kt
@@ -1,36 +1,43 @@
 fun queensAttacktheKing(queens: Array<IntArray>, king: IntArray): List<List<Int>> {
     val kingAttackQueens: MutableList<List<Int>> = mutableListOf()
+    val kingsLocation: MutableSet<Int> = mutableSetOf()
+
+    queens.forEach {
+        kingsLocation.add(it[0] * 11 + it[1])
+    }
 
     val moves: Array<Pair<Int, Int>> = arrayOf(
-        Pair(0,-1),
-        Pair(0,1),
-        Pair(-1,0),
-        Pair(1,0),
+        Pair(0, -1),
+        Pair(0, 1),
+        Pair(-1, 0),
+        Pair(1, 0),
 
-        Pair(-1,-1),
-        Pair(1,-1),
-        Pair(-1,1),
-        Pair(1,1),
+        Pair(-1, -1),
+        Pair(1, -1),
+        Pair(-1, 1),
+        Pair(1, 1),
     )
 
-    for (q in queens){
+    for (q in queens) {
         val x = q[0]
         val y = q[1]
-        var maxMoves = 8
 
-        while (maxMoves > 0){
-          for (m in moves){
-              val xMove = m.first * maxMoves
-              val yMove = m.second * maxMoves
-              if (xMove + x >= 0 && xMove + x < 8
-                  && yMove + y >= 0 && yMove + y < 8
-                  && king[0] == xMove + x && king[1] == yMove + y){
-                  kingAttackQueens.add(
-                      listOf(x,y)
-                  )
-              }
-          }
-           maxMoves--
+        for (m in moves) {
+            var maxMoves = 1
+
+            while (maxMoves < 8) {
+                val xMove = m.first * maxMoves
+                val yMove = m.second * maxMoves
+                val location = (xMove + x) * 11 + (yMove + y)
+                if (kingsLocation.contains(location)) break
+
+                if (xMove + x >= 0 && xMove + x < 8 && yMove + y >= 0 && yMove + y < 8 && king[0] == xMove + x && king[1] == yMove + y) {
+                    kingAttackQueens.add(
+                        listOf(x, y)
+                    )
+                }
+                maxMoves++
+            }
         }
     }
 
@@ -52,7 +59,7 @@ fun main() {
     )
 
     // [[0,1],[1,0],[3,3]]
-    // [[0, 1], [1, 0], [4, 0], [0, 4], [3, 3]]
+    // [[0,1],[1,0],[3,3]]
 
     println(ans.toString())
 }
