86f46a1fb121a640c77ca41aa3c8506d2afe4582
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-06-02 18:06:38 +0500

    2351 question solved

---
+++ src/FirstLetterAppearTwice.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class FirstLetterAppearTwice {
+    public char repeatedCharacter(String s) {
+        Set<Character> set = new HashSet<>();
+        for (char c : s.toCharArray()){
+            if (!set.add(c)){
+                return c;
+            }
+        }
+        return ' ';
+    }
+}
