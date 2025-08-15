ec3221ec0236aef58ec09d398e98baff4fd431fb
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-08-15 18:21:02 +0500

    1089 question solved

---
+++ src/DuplicateZeros.java
@@ -0,0 +1,18 @@
+
+public class DuplicateZeros {
+    public int[] duplicateZeros(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == 0){
+                for (int j = arr.length - 1; j > i; j--){
+                    arr[j] = arr[j - 1];
+                }
+                if (i + 1 < arr.length){
+                    arr[i + 1] = 0;
+                }
+                i++;
+            }
+        }
+        return arr;
+    }
+}
