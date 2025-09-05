1554dcc0d8b969552f739851cabfad804d0c1275
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-09-05 21:46:43 +0500

    1217 question solved

--- src/MinimumCostMoveChipsTheSamePosition.java
+++ src/MinimumCostMoveChipsTheSamePosition.java
@@ -1,19 +1,11 @@
 public class MinimumCostMoveChipsTheSamePosition {
     public int minCostToMoveChips(int[] position) {
-        int index = 0, max = position[0];
-        int minCost = 0;
-
-        for (int i = 0; i < position.length; i++){
-            if (max < position[i]){
-                max = position[i];
-                index = i;
-            }
+        int odd = 0;
+        int even = 0;
+        for(int num : position){
+            if(num % 2 == 0) odd++;
+            else even++;
         }
-
-        for (int i = 0; i < index; i++){
-            if (position[index] - position[i] == 1) minCost++;
-        }
-
-        return minCost;
+        return Math.min(odd,even);
     }
 }
