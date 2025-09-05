4013dc9f6a0a2ea68da5477ad6dbec804f768ba4
sanjarbek <quvandiqovsanjarbek@gmail.com>
2025-09-05 20:51:56 +0500

    1614 question solved

---
+++ src/MaximumNestingDepthParentheses.java
@@ -0,0 +1,14 @@
+public class MaximumNestingDepthParentheses {
+    public int maxDepth(String s) {
+        int max = 0;
+        int count = 0;
+
+        for (char c : s.toCharArray()){
+            if (c == '(') count++;
+            else if (c == ')') count--;
+
+            max = Math.max(max,count);
+        }
+        return max;
+    }
+}
